0d1c4da8ab2e302438e55e7962be9535fdf1604b
mburuanthony <mburuantony@outlook.com>
2022-07-20 11:39:16 +0300

    redone

---
+++ index.js
@@ -0,0 +1,32 @@
+const spinnercontainer = document.querySelector("#spinner_container");
+const scrollbtn = document.querySelector("#scroll");
+const scrollbtntxt = document.querySelector("#btn_text");
+const year = document.querySelector("#year");
+
+const locations = [
+  { id: 0, title: "about" },
+  { id: 1, title: "projects" },
+  { id: 2, title: "tools" },
+  { id: 3, title: "contact" },
+  { id: 4, title: "about" },
+];
+
+let locindex = 1;
+const pagelocation = "#";
+
+scrollbtn.addEventListener("click", () => {
+  if (locindex === 4) locindex = locindex - 4;
+  window.location = pagelocation + locations[locindex].title;
+  scrollbtntxt.textContent = locations[locindex + 1].title;
+
+  locindex += 1;
+});
+
+scrollbtntxt.textContent = locations[1]?.title;
+
+setTimeout(() => {
+  spinnercontainer.style.display = "none";
+}, 4500);
+
+const yeartoday = new Date().getUTCFullYear();
+year.textContent = yeartoday;
