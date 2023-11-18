c2b1c51b946e9925ce1b709d4576d1096bc34702
mburuanthony <mburuantony@outlook.com>
2023-11-18 14:29:07 +0300

    added design projects

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
+const oasisbtn = document.querySelector(".oasisbtn");
 const weathorbtn = document.querySelector(".weathorbtn");
 const autoplaybtn = document.querySelector(".autoplaybtn");
 const countryquizbtn = document.querySelector(".countryquizbtn");
+const veestrmdesignbtn = document.querySelector(".veestrmdesignbtn");
+const autoplaydesignbtn = document.querySelector(".autoplaydesignbtn");
+const oasisdesignbtn = document.querySelector(".oasisdesignbtn");
 
-const projectsids = ["weathor", "autoplay", "countryquiz"];
+const cat_designbtn = document.querySelector(".cat_design");
+const cat_developbtn = document.querySelector(".cat_develop");
 
-const showcurrentproct = (showid, hiddenids) => {
+const yeartext = document.querySelector(".yeartext");
+
+const showcurrentprojt = (showid, hiddenids) => {
   document.querySelector(`#${showid}`).style.cssText =
     "display:block !important;";
 
@@ -14,14 +21,114 @@ const showcurrentproct = (showid, hiddenids) => {
   });
 };
 
+oasisbtn.addEventListener("click", () => {
+  showcurrentprojt("oasisstays", [
+    "weathor",
+    "autoplay",
+    "countryquiz",
+    "oasisstaysdesign",
+    "oasisstaysdesign",
+    "veestream",
+  ]);
+});
+
 weathorbtn.addEventListener("click", () => {
-  showcurrentproct("weathor", ["autoplay", "countryquiz"]);
+  showcurrentprojt("weathor", [
+    "oasisstays",
+    "autoplay",
+    "countryquiz",
+    "oasisstaysdesign",
+    "oasisstaysdesign",
+    "veestream",
+  ]);
 });
 
 autoplaybtn.addEventListener("click", () => {
-  showcurrentproct("autoplay", ["weathor", "countryquiz"]);
+  showcurrentprojt("autoplay", [
+    "oasisstays",
+    "weathor",
+    "countryquiz",
+    "oasisstaysdesign",
+    "oasisstaysdesign",
+    "veestream",
+  ]);
 });
 
 countryquizbtn.addEventListener("click", () => {
-  showcurrentproct("countryquiz", ["weathor", "autoplay"]);
+  showcurrentprojt("countryquiz", [
+    "oasisstays",
+    "weathor",
+    "autoplay",
+    "oasisstaysdesign",
+    "oasisstaysdesign",
+    "veestream",
+  ]);
 });
+
+const showdesignprojects = () => {
+  cat_designbtn.style.cssText = "background-color:#323232";
+  cat_developbtn.style.cssText = "background-color: rgba(38, 38, 38, 0.75)";
+
+  const designprojectsbtn = document.querySelectorAll(".proj_designbtn");
+  const devprojectsbtn = document.querySelectorAll(".proj_developmntbnt");
+
+  devprojectsbtn.forEach((bnt) => {
+    bnt.style.cssText = "display:none !important";
+  });
+
+  designprojectsbtn.forEach((btn) => {
+    btn.style.cssText = "display:flex !important";
+  });
+
+  showcurrentprojt("veestream", [
+    "oasisstays",
+    "weathor",
+    "autoplay",
+    "countryquiz",
+    "oasisstaysdesign",
+    "autoplaydesign",
+  ]);
+};
+
+const showdevelopmentprojectcs = () => {
+  cat_developbtn.style.cssText = "background-color:#323232";
+  cat_designbtn.style.cssText = "background-color: rgba(38, 38, 38, 0.75)";
+
+  const devprojectsbtn = document.querySelectorAll(".proj_developmntbnt");
+  const designprojectsbtn = document.querySelectorAll(".proj_designbtn");
+
+  designprojectsbtn.forEach((btn) => {
+    btn.style.cssText = "display:none !important";
+  });
+
+  devprojectsbtn.forEach((btn) => {
+    btn.style.cssText = "display:flex !important";
+  });
+
+  showcurrentprojt("autoplay", [
+    "weathor",
+    "oasisstays",
+    "countryquiz",
+    "autoplaydesign",
+    "oasisstaysdesign",
+    "veestream",
+  ]);
+};
+
+cat_designbtn.addEventListener("click", () => showdesignprojects());
+cat_developbtn.addEventListener("click", () => showdevelopmentprojectcs());
+
+autoplaydesignbtn.addEventListener("click", () => {
+  showcurrentprojt("autoplaydesign", ["oasisstaysdesign", "veestream"]);
+});
+oasisdesignbtn.addEventListener("click", () => {
+  showcurrentprojt("oasisstaysdesign", ["autoplaydesign", "veestream"]);
+});
+veestrmdesignbtn.addEventListener("click", () => {
+  showcurrentprojt("veestream", ["autoplaydesign", "oasisstaysdesign"]);
+});
+
+showdevelopmentprojectcs();
+
+const date = new Date();
+yeartext.textContent = date.getFullYear();
